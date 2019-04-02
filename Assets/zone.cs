f39f895d8422757d896d18d50a5f31eec422b161
Siren Johansen <sirenjoh@hotmail.com>
2019-04-02 18:19:44 +0200

    Anders Fix it. Playercontroller funker ikke.

--- Assets/zone.cs
+++ Assets/zone.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class zone : MonoBehaviour
 {
     private float Timer;
-    private bool showLabel = false;
+    private bool showTimer = false;
+    private bool showUser = false;
     private float TimeToComplete;
+    private bool playerInBounds;
+    private int playerCounter;
     void Start()
     {
     Timer = Random.Range(5f, 15f); 
-    TimeToComplete = Timer;    
+    TimeToComplete = Timer; 
+    playerInBounds = false; 
+    playerCounter = 0;  
+    }
+
+
+    void OnTriggerEnter(){
+        playerInBounds = true;
+        playerCounter += 1;
     }
 
     void OnTriggerStay(){
-        showLabel = true;
-        Timer -= Time.deltaTime;
-        if (Timer < 0){
+        if (playerCounter >= 2){
+            showTimer = true;
+            Timer -= Time.deltaTime;
+            if (Timer < 0){
             Timer = 0;
             Debug.Log("Run for you life");
+            }
         }
     }
+
 
     void OnTriggerExit(){
         Debug.Log("Exited the zone");
         if (Timer > 0){
+            playerCounter -= 1;
             
             Debug.Log("You left to soon :( Start again");
+            
             
         }
         else {
@@ -34,12 +50,15 @@ public class zone : MonoBehaviour
             Timer = Random.Range(5f, 15f);
 
         }
-        showLabel = false;
+        showTimer = false;
 
     }
     public void OnGUI() {
-         if (showLabel) {
-             GUI.Label(new Rect(10, 10, 100, 20), Timer.ToString());
+         if (showTimer) {
+            GUI.Label(new Rect(10, 10, 100, 20), Timer.ToString());
+         }
+         if (showUser){
+            GUI.Label(new Rect(10, 10, 100, 20), "Missing " + playerCounter.ToString() + " player(s)" );
          }
     }
 }
